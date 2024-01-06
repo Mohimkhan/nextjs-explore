978fe433d9afd6e574bd36e24c3ba6d1f6a6b97b
Mohim Khan <mohimkhan652@gmail.com>
2024-01-06 19:23:31 +0600

    Initial commit (setup the project)

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+          </ul>
+        </nav>
+        {children}
+        </body>
     </html>
   )
 }
